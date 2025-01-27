5ba68211cd53a6b5fa4606327bd85d098a77ba85
Hoa Truong <minhanhoa.truong@cynergi.io>
2025-01-27 20:14:26 +0700

    Init project

---
+++ selenium/grid/BaseTest.java
@@ -0,0 +1,71 @@
+package grid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class BaseTest {
+	WebDriver driver;
+	String projectFolder = System.getProperty("user.dir");
+
+	protected WebDriver getBrowserDriver(String browserName, String url, String osName, String ipAddress, String portNumber) {
+		DesiredCapabilities capability = null;
+		Platform platform = null;
+
+		if (osName.toLowerCase().contains("windows")) {
+			platform = Platform.WINDOWS;
+		} else {
+			platform = Platform.MAC;
+		}
+
+		switch (browserName) {
+		case "firefox":
+			capability = DesiredCapabilities.firefox();
+			capability.setBrowserName("firefox");
+//			capability.setVersion("0.34");
+			capability.setPlatform(platform);
+
+			FirefoxOptions fOptions = new FirefoxOptions();
+			fOptions.merge(capability);
+			break;
+		case "chrome":
+			capability = DesiredCapabilities.chrome();
+			capability.setBrowserName("chrome");
+			capability.setPlatform(platform);
+
+			ChromeOptions cOptions = new ChromeOptions();
+			cOptions.merge(capability);
+			break;
+		case "edge":
+			capability = DesiredCapabilities.edge();
+			capability.setBrowserName("MicrosoftEdge");
+			capability.setPlatform(platform);
+
+			EdgeOptions eOptions = new EdgeOptions();
+			eOptions.merge(capability);
+			break;
+		default:
+			throw new RuntimeException("Browser is not valid!");
+		}
+
+		try {
+			driver = new RemoteWebDriver(new URL(String.format("http://%s:%s/wd/hub", ipAddress, portNumber)), capability);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(url);
+		return driver;
+	}
+
+}
