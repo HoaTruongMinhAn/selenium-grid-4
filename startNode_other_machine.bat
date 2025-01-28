addc59dc5bed5b27c3e1244b1049e392b23d71a9
Hoa Truong <minhanhoa.truong@cynergi.io>
2025-01-28 18:02:44 +0700

    Demo Selenium Grid with real machine and virtual machine

--- startNode_other_machine.bat
+++ startNode_other_machine.bat
@@ -1,2 +1,3 @@
 set ProjectPath=%~dp0
-java -jar "%ProjectPath%selenium-server-4.28.1.jar" node --hub http://192.168.1.8:4444
+java -jar "%ProjectPath%selenium-server-4.28.1.jar" node --hub http://192.168.1.8:4444 --selenium-manager true
+pause
--- startNode_same_machine.bat
+++ startNode_same_machine.bat
@@ -1,3 +1,3 @@
 set ProjectPath=%~dp0
-java -jar "%ProjectPath%\libraries\selenium-server-4.28.1.jar" node
+java -jar "%ProjectPath%\libraries\selenium-server-4.28.1.jar" node --selenium-manager true
 pause
